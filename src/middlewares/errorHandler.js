40429a39d04f4a63756be00edd38d1d486072430
AnnaSofiya1 <annasofiya1111@gmail.com>
2024-08-12 11:48:46 +0300

    data

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -17,8 +17,9 @@ export const errorHandler = (err, req, res, next) => {
         res.status(err.status).json({
             status: err.status,
             message: err.name,
-            data: {
+            data: err.data || {
                 message: err.message,
+                data: err,
             },
         });
         return;
